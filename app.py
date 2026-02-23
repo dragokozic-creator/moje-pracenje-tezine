ce22efed66b21413a0ac0b210ba5f5e071d51066
dragokozic-creator <drago.kozic@gmail.com>
2026-02-23 19:01:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ st.title("⚖️ Moj Pratioc Težine")
 
 # Inicijalizacija baze podataka (jednostavna tablica)
 if 'data' not in st.session_state:
-    st.session_state.data = pd.DataFrame(columns=["Datum", "Težina (kg)"])
+    st.session_state.data = pd.DataFrame(columns=["Datum", "Težina (kg)", "lokacja"])
 
 # --- UNOS PODATAKA ---
 with st.sidebar:
     st.header("Novi unos")
     tezina = st.number_input("Unesi težinu (kg):", min_value=10.0, max_value=300.0, step=0.1)
     datum = st.date_input("Datum:", datetime.date.today())
+    lokacija = st.text_input("Lokacija:")
     
     if st.button("Spremi"):
         novi_unos = pd.DataFrame({"Datum": [datum], "Težina (kg)": [tezina]})
